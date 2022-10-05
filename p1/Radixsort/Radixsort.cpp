13d198d4b0fb0e6d184e869347144c6b638cc5fc
nerea <801950@unizar.es>
2022-10-05 07:41:36 -0700

    radix

--- p1/Radixsort/Radixsort.cpp
+++ p1/Radixsort/Radixsort.cpp
@@ -1,10 +1,12 @@
 #include "Radixsort.hpp"
 
+// Cuenta la cantidad de dígitos de un número
 int countDigits(int n){
     string aux = to_string(n);
     return aux.size();
 }
 
+// Devuelve 
 int maxCifras(int arr[], int n)
 {
     int mx = arr[0];
@@ -16,15 +18,13 @@ int maxCifras(int arr[], int n)
 
 void sort (int arr[], int exp, int n){
     int output[n], count[10] = {0};
-
     for (int i = 0; i < n; i++){
-    //    cout << arr[i] << " " << (arr[i]/exp)%10 << endl;
         count[(arr[i]/exp)%10]++;
     }
 
     for(int i = 1; i < 10; i ++){
         count[i] += count[i-1]; 
-    //    cout << count[i] << " ";
+        cout << count[i] << " " << endl;
     }
  //   cout << endl;
 
@@ -41,7 +41,7 @@ void sort (int arr[], int exp, int n){
 void radixsort(int arr[], int n){
     int max = maxCifras(arr, n);
     
-    for (int exp = 1 ; exp > 0; exp *= 10){
+    for (int exp = 1 ; max/exp > 0; exp *= 10){
         sort(arr, exp, n);
      //   printVector(arr,n);
     }
@@ -64,28 +64,27 @@ void readData(string filepath, list<int>& out) {
 
 }
 
-void printArray(int arr[], int n){
-    for(int i = 0; i < n; i ++){
-        cout << i << ": " << arr[i] << endl ;
-    }
-    cout << endl;
-}
+// void printArray(int arr[], int n){
+//     for(int i = 0; i < n; i ++){
+//         cout << i << ": " << arr[i] << endl ;
+//     }
+//     cout << endl;
+// }
 
-int main(int argc, char *argv[]){
-    // int n = 10;
-    // int vector[n] = {4 , 85, 6 , 14, 98, 100, 8789, 234, 024, 45756};
-    std::list<int> numbers = std::list<int>();
-    string filepath = "chiki.txt";
-    readData(filepath, numbers);
+// int main(int argc, char *argv[]){
+//     // int n = 10;
+//     // int vector[n] = {4 , 85, 6 , 14, 98, 100, 8789, 234, 024, 45756};
+//     std::list<int> numbers = std::list<int>();
+//     string filepath = "chiki.txt";
+//     readData(filepath, numbers);
 
-    int n = numbers.size();
-    int arr[n];
-    int k = 0;
-    for (int const &i: numbers) {
-        arr[k++] = i;
-    }
-
-    radixsort(arr, n);
-    printArray(arr, n);
-    return 0;
-}
+//     int n = numbers.size();
+//     int arr[n];
+//     int k = 0;
+//     for (int const &i: numbers) {
+//         arr[k++] = i;
+//     }
+//     radixsort(arr, n);
+//     printArray(arr, n);
+//     return 0;
+// }
