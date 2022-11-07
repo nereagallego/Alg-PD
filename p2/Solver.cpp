438964ffda8d8ef4d5f626b18077cf59125c7c69
nerea <801950@unizar.es>
2022-11-07 02:47:27 -0800

    add functions

--- p2/Solver.cpp
+++ p2/Solver.cpp
@@ -2,21 +2,28 @@
 
 Solver::Solver(){
     initVariables();
-  //  one_square_one_value();
-  //  non_duplicated_values();
+    unValorPorCelda();
+    noValoresDuplicados();
+}
+
+Minisat::Var toVar(int row, int column, int value) {
+    assert(row >= 0 && row < _rows    && "Attempt to get var for nonexistant row");
+    assert(column >= 0 && column < _columns && "Attempt to get var for nonexistant column");
+    assert(value >= 0 && value < _values   && "Attempt to get var for nonexistant value");
+    return row * _columns * _values + column * _values + value;
 }
 
 void Solver::initVariables(){
-    for (int i = 0; i < rows; ++i) {
-      for (int j = 0; j < columns; ++j) {
-        for (int v = 0; v < values; ++v) {
+    for (int i = 0; i < _rows; ++i) {
+      for (int j = 0; j < _columns; ++j) {
+        for (int v = 0; v < _values; ++v) {
           static_cast<void>(resolutor.newVar());
         }
       }
     }
 }
 
-void Solver::unValorPorCelda(Minisat::vec<Minisat::Lit> const& literales){
+void Solver::exactamenteUnValor(Minisat::vec<Minisat::Lit> const& literales){
     resolutor.addClause(literales);
     for (size_t i = 0; i < literales.size(); ++i) {
         for (size_t j = i + 1; j < literales.size(); ++j) {
@@ -26,5 +33,53 @@ void Solver::unValorPorCelda(Minisat::vec<Minisat::Lit> const& literales){
 }
 
 void Solver::noValoresDuplicados(){
-    
+    for (int row = 0; row < _rows; ++row) {
+        for (int value = 0; value < _values; ++value) {
+            Minisat::vec<Minisat::Lit> literals;
+            for (int column = 0; column < _columns; ++column) {
+                literals.push(Minisat::mkLit(toVar(row, column, value)));
+            }
+            exactamenteUnValor(literals);
+        }
+    }
+    // In each column, for each value, forbid two rows sharing that value
+    for (int column = 0; column < _columns; ++column) {
+        for (int value = 0; value < _values; ++value) {
+            Minisat::vec<Minisat::Lit> literals;
+            for (int row = 0; row < _rows; ++row) {
+                literals.push(Minisat::mkLit(toVar(row, column, value)));
+            }
+            exactamenteUnValor(literals);
+        }
+    }
+    // Now forbid sharing in the 3x3 boxes
+    for (int r = 0; r < 9; r += 3) {
+        for (int c = 0; c < 9; c += 3) {
+            for (int value = 0; value < _values; ++value) {
+                Minisat::vec<Minisat::Lit> literals;
+                for (int rr = 0; rr < 3; ++rr) {
+                    for (int cc = 0; cc < 3; ++cc) {
+                        literals.push(Minisat::mkLit(toVar(r + rr, c + cc, value)));
+                    }
+                }
+                exactamenteUnValor(literals);
+            }
+        }
+    }
+}
+
+void Solver::unValorPorCelda() {
+    for (int row = 0; row < _rows; ++row) {
+        for (int column = 0; column < _columns; ++column) {
+            Minisat::vec<Minisat::Lit> literals;
+            for (int value = 0; value < _values; ++value) {
+                literals.push(Minisat::mkLit(toVar(row, column, value)));
+            }
+            exactamenteUnValor(literals);
+        }
+    }
+}
+
+bool Solver::solve() {
+    return resolutor.solve();
 }
--- p2/Solver.hpp
+++ p2/Solver.hpp
@@ -2,11 +2,12 @@
 
 #include "minisat-master/minisat/core/Solver.h"
 
+static const int _rows = 9;
+static const int _columns = 9;
+static const int _values = 9;
 class Solver{
     Minisat::Solver resolutor;
-    const int rows = 9;
-    const int columns = 9;
-    const int values = 9;
+    
 public:
 
     Solver();
@@ -15,8 +16,14 @@ public:
 
     // ∀(i,j) ∈ {filas x columnas} : solo puede haber un valor por celda
     // ∀(v1,v2) ∈ {0..9,0..9} ~v1 ∨ ~v2
-    void unValorPorCelda(Minisat::vec<Minisat::Lit> const& literales);
+    void exactamenteUnValor(Minisat::vec<Minisat::Lit> const& literales);
 
     // no hay valores duplicador
     void noValoresDuplicados();
+
+    // comprueba que haya al menos un valor en cada celda
+    void unValorPorCelda();
+
+    bool solve();
+
 };
